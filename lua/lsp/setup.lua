1ba211d4e1c775bc72d6820ad4437ee70553f54b
fys <1113014250@qq.com>
2022-05-07 11:55:05 +0800

    1.add todo update nvim-installer lsp config 2.simple support golang 3. fix nvim-installer config err(not install lsp server)

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -4,10 +4,11 @@ local lsp_installer = require("nvim-lsp-installer")
 local servers = {
   sumneko_lua = require("lsp.config.lua"),
   rust_analyzer = require("lsp.config.rust"),
+  gopls = require("lsp.config.go")
 }
 
 ------------------ install language server ---------------------
-for _, name in pairs(servers) do
+for name, config in pairs(servers) do
   local server_is_found, server = lsp_installer.get_server(name)
   if server_is_found and not server:is_installed() then
     print("Installing " .. name)
@@ -17,6 +18,7 @@ end
 
 
 ------------------ language server config ---------------------
+-- todo: https://github.com/williamboman/nvim-lsp-installer/discussions/636
 lsp_installer.on_server_ready(function (server)
   local config = servers[server.name]
   if config == nil then
