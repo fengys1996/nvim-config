a114ab50588a2b1c29ffab528253697139f2feb2
fys <1113014250@qq.com>
2022-08-07 01:03:46 +0800

    rename opts to server_opts

--- lua/lsp/config/rust.lua
+++ lua/lsp/config/rust.lua
@@ -1,4 +1,4 @@
-local opts = {
+local server_opts = {
   flags = {
     debounce_text_changes = 150,
   },
@@ -18,13 +18,15 @@ local opts = {
 
 return {
   on_setup = function(server)
+
     -- Initialize the LSP via rust-tools instead
     require("rust-tools").setup({
       -- The "server" property provided in rust-tools setup function are the
       -- settings rust-tools will provide to lspconfig during init.            --
       -- We merge the necessary settings from nvim-lsp-installer (server:get_default_options())
       -- with the user's own settings (opts).
-      server = vim.tbl_deep_extend("force", server:get_default_options(), opts),
+      server = vim.tbl_deep_extend("force", server:get_default_options(), server_opts),
+
       -- dap = {
       --   adapter = require("rust-tools.dap").get_codelldb_adapter(
       --     "/home/simrat39/.vscode/extensions/vadimcn.vscode-lldb-1.6.7/adapter/codelldb",
