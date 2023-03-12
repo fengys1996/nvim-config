6bb9851fa73d57a4305eaf70ee6a531c4e3e1750
fys <1113014250@qq.com>
2023-03-12 16:13:36 +0800

    support chatgpt

---
+++ lua/plugins/chatgpt.lua
@@ -0,0 +1,25 @@
+local config = {
+	keymaps = {
+		close = { "<C-c>" },
+		submit = "<C-s>",
+		yank_last = "<C-y>",
+		scroll_up = "<C-u>",
+		scroll_down = "<C-d>",
+		toggle_settings = "<C-o>",
+		new_session = "<C-n>",
+		cycle_windows = "<Tab>",
+		-- in the Sessions pane
+		select_session = "<Space>",
+		rename_session = "r",
+		delete_session = "d",
+	},
+};
+
+return {
+	{
+		"jackMort/ChatGPT.nvim",
+		event = "VeryLazy",
+		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+		config = config,
+	},
+}
