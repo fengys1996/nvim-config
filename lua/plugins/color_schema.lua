7fbe4e1ada0aa115c303c55e5738bc5a8c48149e
fys <1113014250@qq.com>
2023-05-30 10:07:10 +0800

    make the background transparent

--- lua/plugins/color_schema.lua
+++ lua/plugins/color_schema.lua
@@ -9,7 +9,7 @@ return {
 		config = function()
 			vim.g.nord_contrast = false
 			vim.g.nord_borders = false
-			vim.g.nord_disable_background = false
+			vim.g.nord_disable_background = true
 			vim.g.nord_italic = false
 			vim.g.nord_uniform_diff_background = true
 			vim.g.nord_bold = false
