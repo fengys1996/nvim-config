33a6c56295de5576b33bc3b4cd938b38b70afcd5
fys <1113014250@qq.com>
2023-08-01 16:38:28 +0800

    add "octo" plugin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -99,5 +99,10 @@ return {
 		event = "VeryLazy",
 		config = function()
 		end,
+	},
+	{
+		'pwntester/octo.nvim',
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim", "kyazdani42/nvim-web-devicons" },
+		config = true,
 	}
 }
