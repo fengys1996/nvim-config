3fa8ec54eee982a850cbff38a32dfec6a9dad73d
fys <fengys1996@gmail.com>
2024-09-01 11:21:49 +0800

    add rgflow.nvim plugin

---
+++ lua/plugins/rg.lua
@@ -0,0 +1,15 @@
+return {
+
+	{
+		"mangelozzi/rgflow.nvim",
+		config = function()
+			require('rgflow').setup
+			{
+				cmd_flags = "--smart-case --fixed-strings --ignore --max-columns 200",
+				default_trigger_mappings = true,
+				default_ui_mappings = true,
+				default_quickfix_mappings = true,
+			}
+		end
+	},
+}
