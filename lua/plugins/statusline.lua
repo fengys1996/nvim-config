b4e79ddab7520320dc715bab21e46ab6dae1a10e
fys <fengys1996@gmail.com>
2025-02-20 21:35:43 +0800

    add opts for lualine

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,7 +1,25 @@
+local lualine_opts = {
+	sections = {
+		lualine_c = {
+			{
+				'filename',
+				file_status = true,
+				path = 2,
+				short_target = 40,
+				symbols = {
+					modified = '[+]',
+					readonly = '[-]',
+					unnamed = '[No Name]',
+				}
+			},
+		},
+	},
+};
+
 return {
 	{
 		'nvim-lualine/lualine.nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
-		opts = true,
+		opts = lualine_opts,
 	}
 }
