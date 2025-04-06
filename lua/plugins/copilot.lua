b9b44798f92229875366b15f7cdff78020a03ad4
fys <fengys1996@gmail.com>
2025-04-06 08:03:46 +0800

    chore: add next suggestion keymap

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,6 @@
+-- vim.keymap.set('i', '<C-l>', '<Plug>(copilot-accept-word)')
+-- Cycle to the next suggestion, if one is available.
+vim.keymap.set('i', '<C-j>', '<Plug>(copilot-next)')
 return {
 	{
 		"github/copilot.vim",
