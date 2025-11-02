7dab2ee1f9505f224b892609a2e29812ea68cd31
fys <fengys1996@gmail.com>
2025-11-02 19:20:10 +0800

    chore: remove some color schemas

--- init.lua
+++ init.lua
@@ -1,12 +1,12 @@
 local core_conf_files = {
-	"globals.vim",
+    "globals.vim",
 }
 
 -- source all the core config files
 for _, name in ipairs(core_conf_files) do
-	local path = string.format("%s/vimscript/%s", vim.fn.stdpath('config'), name)
-	local source_cmd = "source " .. path
-	vim.cmd(source_cmd)
+    local path = string.format("%s/vimscript/%s", vim.fn.stdpath('config'), name)
+    local source_cmd = "source " .. path
+    vim.cmd(source_cmd)
 end
 
 require("basic")
@@ -14,53 +14,45 @@ require("keymap.basic")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	spec = {
-		{ import = "plugins" },
+    spec = {
+        { import = "plugins" },
 
-	},
-	change_detection = {
-		enabled = false,
-		notify = false,
-	},
-	checker = {
-		enabled = false,
-		notify = false,
-	},
+    },
+    change_detection = {
+        enabled = false,
+        notify = false,
+    },
+    checker = {
+        enabled = false,
+        notify = false,
+    },
 })
 
--- Options:
--- nightfox, dayfox, nordfox, dawnfox
--- nordic
--- habamax
--- vscode
--- tokyonight-day
--- kanagawa
--- vim.o.background = "light"
--- vim.o.background = "dark"
--- vim.cmd("colorscheme gruvbox")
-vim.cmd("colorscheme dayfox")
--- vim.cmd("colorscheme everforest")
--- vim.cmd("colorscheme kanagawa-lotus")
+vim.o.background = "dark"
+vim.cmd("colorscheme everforest")
+if vim.g.colors_name == "everforest" then
+    vim.cmd("hi SignColumn ctermbg=59 guibg=#505050")
+end
+-- Since https://github.com/neovim/neovim/pull/32383
+vim.cmd("hi link qftext LineNr")
+
 
 require("cmd.base")
 
 require("custom.colorcolumn").setup()
-
--- Since https://github.com/neovim/neovim/pull/32383
-vim.cmd("hi link qftext LineNr")
 
 vim.o.runtimepath = vim.o.runtimepath .. "," .. "/home/fys/.config/nvim-rs"
 require("neovide")
--- lua/plugins/color_schema.lua
+++ lua/plugins/color_schema.lua
@@ -1,112 +1,52 @@
-local tokyonight_opt = {
-	transparent = false,
-	terminal_colors = true,
-}
-
-local function tokyonight_config()
-	require("tokyonight").setup(tokyonight_opt);
-end
-
-local function vscode_config()
-	local c = require('vscode.colors').get_colors()
-	require('vscode').setup({
-		style = 'light',
-		transparent = false,
-		italic_comments = true,
-		disable_nvimtree_bg = true,
-		color_overrides = {
-			vscLineNumber = '#FFFFFF',
-		},
-		group_overrides = {
-			Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
-		}
-	})
-	require('vscode').load()
-end
-
-local function nord_config()
-	vim.g.nord_contrast = false
-	vim.g.nord_borders = false
-	vim.g.nord_disable_background = false
-	vim.g.nord_italic = false
-	vim.g.nord_uniform_diff_background = true
-	vim.g.nord_bold = false
-end
-
 local function everforest_config()
-	require("everforest").setup({
-		-- options are "soft", "medium" or "hard".
-		background = "medium",
-		transparent_background_level = 0,
-		italics = false,
-		disable_italic_comments = true,
-		sign_column_background = "none",
-		on_highlights = function(hl, palette)
-			hl.NvimTreeCursorLine = { fg = palette.None, bg = palette.gray, sp = palette.none }
-			hl.NvimTreeNormal = { fg = palette.none, bg = palette.none, sp = palette.none }
-			hl.NvimTreeEndOfBuffer = { fg = palette.none, bg = palette.none, sp = palette.none }
-		end,
-	})
+    require("everforest").setup({
+        -- Options "soft"/"medium"/"hard".
+        background = "medium",
+        transparent_background_level = 0,
+        italics = false,
+        disable_italic_comments = true,
+        sign_column_background = "none",
+        on_highlights = function(hl, palette)
+            hl.NvimTreeCursorLine = { fg = palette.None, bg = palette.gray, sp = palette.none }
+            hl.NvimTreeNormal = { fg = palette.none, bg = palette.none, sp = palette.none }
+            hl.NvimTreeEndOfBuffer = { fg = palette.none, bg = palette.none, sp = palette.none }
+        end,
+    })
 end
 
 return {
-	{
-		"ellisonleao/gruvbox.nvim",
-		enabled = true,
-		lazy = false,
-		priority = 1000,
-		config = true
-	},
-	{
-		"fengys1996/nightfox.nvim",
-		enabled = true,
-		lazy = false,
-		priority = 1000,
-		config = true
-	},
-	{
-		'AlexvZyl/nordic.nvim',
-		enabled = true,
-		lazy = true,
-		config = true
-	},
-	{
-		"Mofiqul/vscode.nvim",
-		enabled = true,
-		lazy = true,
-		config = vscode_config
-	},
-	{
-		"fengys1996/nord.nvim",
-		enabled = true,
-		lazy = true,
-		config = nord_config
-	},
-	-- tokyonight
-	{
-		"folke/tokyonight.nvim",
-		enabled = true,
-		lazy = true,
-		config = tokyonight_config
-	},
-	-- kanagawa
-	{
-		"rebelot/kanagawa.nvim",
-		enabled = true,
-		lazy = false,
-		priority = 1000,
-		config = true
-	},
-	-- everforest
-	{
-		"neanias/everforest-nvim",
-		lazy = false,
-		priority = 1000,
-		config = everforest_config
-	},
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-	},
+    {
+        "ellisonleao/gruvbox.nvim",
+        enabled = true,
+        lazy = false,
+        priority = 1000,
+        config = true
+    },
+    {
+        "fengys1996/nightfox.nvim",
+        enabled = true,
+        lazy = false,
+        priority = 1000,
+        config = true
+    },
+    -- kanagawa
+    {
+        "rebelot/kanagawa.nvim",
+        enabled = true,
+        lazy = false,
+        priority = 1000,
+        config = true
+    },
+    -- everforest
+    {
+        "neanias/everforest-nvim",
+        lazy = false,
+        priority = 1000,
+        config = everforest_config
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+    },
 }
