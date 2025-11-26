55b1e3a8a9742588547a69140a0dce06bdbb2c95
fys <fengys1996@gmail.com>
2025-11-26 19:15:07 +0800

    upgrade crates.nvim version and disable when start

--- lua/cmd/base.lua
+++ lua/cmd/base.lua
@@ -2,38 +2,32 @@ vim.api.nvim_create_user_command('Proj', 'Telescope projects', {})
 vim.api.nvim_create_user_command('Projm', 'edit ~/.local/share/nvim/project_nvim/project_history', {})
 
 local function live_grep_in_path(path)
-	local _path = path or vim.fn.input("Dir: ", "", "dir")
-	require("telescope.builtin").live_grep({ search_dirs = { _path } })
+    local _path = path or vim.fn.input("Dir: ", "", "dir")
+    require("telescope.builtin").live_grep({ search_dirs = { _path } })
 end
 
 vim.api.nvim_create_user_command('Rgpath', live_grep_in_path, {})
 
 -- Toggle the crates.
-vim.api.nvim_create_user_command('Crates', function(opt)
-	if string.lower(opt.args) == "toggle" then
-		require("crates").toggle()
-	end
-end, {
-	nargs = 1,
-	complete = function()
-		return { 'toggle' }
-	end,
-})
+local function toggle_crates()
+    require('crates').toggle()
+end
+vim.api.nvim_create_user_command('ToggleCrates', toggle_crates, {})
 
 vim.api.nvim_create_user_command('ToggleInlayHint',
-	'lua vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())', {})
+    'lua vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())', {})
 
 -- Custom lsp start command  `HaloStart`
 --
 -- Why do we need this?
 -- Since there are some language servers that need to be started in a different way.
 local function start_lsp()
-	if vim.bo.filetype == "rust" then
-		require('rustaceanvim.lsp').start()
-		require('plugins.lsp.rust').enable_auto_attach()
-	else
-		vim.cmd("LspStart")
-	end
+    if vim.bo.filetype == "rust" then
+        require('rustaceanvim.lsp').start()
+        require('plugins.lsp.rust').enable_auto_attach()
+    else
+        vim.cmd("LspStart")
+    end
 end
 
 vim.api.nvim_create_user_command('HaloStart', start_lsp, {})
@@ -43,22 +37,22 @@ vim.api.nvim_create_user_command('HaloStart', start_lsp, {})
 -- Why do we need this?
 -- Since there are some language servers that need to be stopped in a different way.
 local function stop_lsp()
-	if vim.bo.filetype == "rust" then
-		require('rustaceanvim.lsp').stop()
-		require('plugins.lsp.rust').disable_auto_attach()
-	else
-		vim.cmd("LspStop")
-	end
+    if vim.bo.filetype == "rust" then
+        require('rustaceanvim.lsp').stop()
+        require('plugins.lsp.rust').disable_auto_attach()
+    else
+        vim.cmd("LspStop")
+    end
 end
 
 vim.api.nvim_create_user_command('HaloStop', stop_lsp, {})
 
 vim.api.nvim_create_user_command(
-	'Browse',
-	function(opts)
-		vim.fn.system { 'xdg-open', opts.fargs[1] }
-	end,
-	{ nargs = 1 }
+    'Browse',
+    function(opts)
+        vim.fn.system { 'xdg-open', opts.fargs[1] }
+    end,
+    { nargs = 1 }
 )
 
 -- Autocmds
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,52 +1,55 @@
 return {
-	-- {
-	-- 	"neovim/nvim-lspconfig",
-	-- 	dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig" },
-	-- 	config = function()
-	-- 	end
-	-- },
-	{
-		"mfussenegger/nvim-jdtls",
-		  ft = { 'java' },
-	},
-	{
-		-- 'mrcjkb/rustaceanvim',
-		'fengys1996/rustaceanvim',
-		branch = 'fix/standalone-support',
-		-- dir = '/home/fys/projects/rustaceanvim',
-		-- version = '^6',
-		ft = { 'rust' },
-		config = function()
-			require('plugins.lsp.rust').setup()
-		end
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = { "williamboman/mason.nvim" },
-		opts = {
-			-- rust-analyzer is managed by rustup, different toolchains have different rust-analyzers.
-			-- ensure_installed = { "lua_ls", "taplo", "gopls", "clangd" }
-		}
+    -- {
+    -- 	"neovim/nvim-lspconfig",
+    -- 	dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig" },
+    -- 	config = function()
+    -- 	end
+    -- },
+    {
+        "mfussenegger/nvim-jdtls",
+        ft = { 'java' },
+    },
+    {
+        -- 'mrcjkb/rustaceanvim',
+        'fengys1996/rustaceanvim',
+        branch = 'fix/standalone-support',
+        -- dir = '/home/fys/projects/rustaceanvim',
+        -- version = '^6',
+        ft = { 'rust' },
+        config = function()
+            require('plugins.lsp.rust').setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "williamboman/mason.nvim" },
+        opts = {
+            -- rust-analyzer is managed by rustup, different toolchains have different rust-analyzers.
+            -- ensure_installed = { "lua_ls", "taplo", "gopls", "clangd" }
+        }
 
-	},
-	{
-		"williamboman/mason.nvim",
-		cmd = "Mason",
-		opts = {
-			-- install_root_dir = require("config").language_server_bin_path,
-			-- PATH = "append",
-			registries = {
-				-- https://github.com/nvim-java/nvim-java/issues/315
-				'github:nvim-java/mason-registry',
-				'github:mason-org/mason-registry',
-			}
-		}
-	},
-	{
-		"saecki/crates.nvim",
-		dependencies = "nvim-lua/plenary.nvim",
-		event = "VeryLazy",
-		tag = "v0.6.0",
-		config = true,
-	},
+    },
+    {
+        "williamboman/mason.nvim",
+        cmd = "Mason",
+        opts = {
+            -- install_root_dir = require("config").language_server_bin_path,
+            -- PATH = "append",
+            registries = {
+                -- https://github.com/nvim-java/nvim-java/issues/315
+                'github:nvim-java/mason-registry',
+                'github:mason-org/mason-registry',
+            }
+        }
+    },
+    {
+        "saecki/crates.nvim",
+        dependencies = "nvim-lua/plenary.nvim",
+        event = { "BufRead Cargo.toml" },
+        tag = "v0.7.1",
+        config = function ()
+            require('crates').setup({})
+            require('crates').hide()
+        end
+    },
 }
