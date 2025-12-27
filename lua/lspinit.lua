8dba2cadfc726af34842366d10d486ee3905b286
fys <fengys1996@gmail.com>
2025-12-27 23:03:23 +0800

    avoid conflicts

--- lua/lspinit.lua
+++ lua/lspinit.lua
@@ -39,7 +39,7 @@ local function maplsp(bufnr, lsp_name)
         vim.lsp.buf.format({ async = true })
     end, opts)
     -- signature help
-    vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
     if is_rust_analyzer(lsp_name) then
         -- the special rust-analyzer keymaps
         vim.keymap.set('n', 'gh', '<cmd>RustLsp hover actions<CR>', opts)
