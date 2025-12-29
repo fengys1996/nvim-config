ec253e9f348376f5057f726819af740c78c537cb
fys <fengys1996@gmail.com>
2025-12-29 13:38:02 +0800

    config diffview

--- init.lua
+++ init.lua
@@ -6,9 +6,9 @@ require("lazy_c")
 -- vim.o.background = "light"
 -- vim.o.background = "background"
 
-vim.cmd("colorscheme onelight")
+-- vim.cmd("colorscheme onelight")
 -- vim.cmd("colorscheme everforest")
--- vim.cmd("colorscheme dayfox")
+vim.cmd("colorscheme dayfox")
 -- vim.cmd("colorscheme gruvbox")
 
 -- Since https://github.com/neovim/neovim/pull/32383
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,94 +1,109 @@
 local gitsigns_opts = {
-	signs = {
-		add          = { text = '+' },
-		change       = { text = '~' },
-		delete       = { text = '_' },
-		topdelete    = { text = '‾' },
-		changedelete = { text = '-' },
-		untracked    = { text = '┆' },
-	},
-	signs_staged = {
-		add          = { text = 's' },
-		change       = { text = 's' },
-		delete       = { text = 's' },
-		topdelete    = { text = 's' },
-		changedelete = { text = 's' },
-		untracked    = { text = 's' },
-	},
-	sign_priority = 6,
-	on_attach = function(bufnr)
-		local gitsigns = require('gitsigns')
+    signs = {
+        add          = { text = '+' },
+        change       = { text = '~' },
+        delete       = { text = '_' },
+        topdelete    = { text = '‾' },
+        changedelete = { text = '-' },
+        untracked    = { text = '┆' },
+    },
+    signs_staged = {
+        add          = { text = 's' },
+        change       = { text = 's' },
+        delete       = { text = 's' },
+        topdelete    = { text = 's' },
+        changedelete = { text = 's' },
+        untracked    = { text = 's' },
+    },
+    sign_priority = 6,
+    on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
 
-		local function map(mode, l, r, opts)
-			opts = opts or {}
-			opts.buffer = bufnr
-			vim.keymap.set(mode, l, r, opts)
-		end
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
 
-		-- Navigation
-		map('n', ']c', function()
-			if vim.wo.diff then
-				vim.cmd.normal({ ']c', bang = true })
-			else
-				gitsigns.nav_hunk('next')
-			end
-		end)
+        -- Navigation
+        map('n', ']c', function()
+            if vim.wo.diff then
+                vim.cmd.normal({ ']c', bang = true })
+            else
+                gitsigns.nav_hunk('next')
+            end
+        end)
 
-		map('n', '[c', function()
-			if vim.wo.diff then
-				vim.cmd.normal({ '[c', bang = true })
-			else
-				gitsigns.nav_hunk('prev')
-			end
-		end)
+        map('n', '[c', function()
+            if vim.wo.diff then
+                vim.cmd.normal({ '[c', bang = true })
+            else
+                gitsigns.nav_hunk('prev')
+            end
+        end)
 
-		-- Actions
-		map('n', '<leader>hs', gitsigns.stage_hunk)
-		map('n', '<leader>hr', gitsigns.reset_hunk)
-		map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
-		map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
-		map('n', '<leader>hS', gitsigns.stage_buffer)
-		map('n', '<leader>hu', gitsigns.undo_stage_hunk)
-		map('n', '<leader>hR', gitsigns.reset_buffer)
-		map('n', '<leader>hp', gitsigns.preview_hunk)
-		map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end)
-		map('n', '<leader>hd', gitsigns.diffthis)
-		map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
-		-- map('n', '<leader>td', gitsigns.toggle_deleted)
-	end
+        -- Actions
+        map('n', '<leader>hs', gitsigns.stage_hunk)
+        map('n', '<leader>hr', gitsigns.reset_hunk)
+        map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+        map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+        map('n', '<leader>hS', gitsigns.stage_buffer)
+        map('n', '<leader>hu', gitsigns.undo_stage_hunk)
+        map('n', '<leader>hR', gitsigns.reset_buffer)
+        map('n', '<leader>hp', gitsigns.preview_hunk)
+        map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end)
+        map('n', '<leader>hd', gitsigns.diffthis)
+        map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
+        -- map('n', '<leader>td', gitsigns.toggle_deleted)
+    end
 }
 
+local diffview = function()
+    local actions = require("diffview.actions")
+    require("diffview").setup({
+        keymaps = {
+            disable_defaults = false,
+            file_panel = {
+                { "n", "<A-m>",      actions.toggle_files,                  { desc = "Toggle the file panel." } },
+            },
+            view = {
+                { "n", "<A-m>",      actions.toggle_files,                  { desc = "Toggle the file panel." } },
+            },
+        }
+    })
+end
+
 return {
-	{
-		"lewis6991/gitsigns.nvim",
-		tag = "v1.0.2",
-		event = "VeryLazy",
-		config = function()
-			require('gitsigns').setup(gitsigns_opts)
-		end,
-	},
-	{
-		"tpope/vim-fugitive",
-		tag = "v3.7",
-		event = "VeryLazy",
-		config = function()
-		end,
-	},
-	{
-		"junegunn/gv.vim",
-		event = "VeryLazy",
-		config = function()
-		end,
-	},
-	{
-		"sindrets/diffview.nvim",
-		event = "VeryLazy",
-		config = true,
-	},
-	{
-		"tpope/vim-rhubarb",
-		event = "VeryLazy",
-		config = function()
-		end,
-	}
+    {
+        "lewis6991/gitsigns.nvim",
+        tag = "v1.0.2",
+        event = "VeryLazy",
+        config = function()
+            require('gitsigns').setup(gitsigns_opts)
+        end,
+    },
+    {
+        "tpope/vim-fugitive",
+        tag = "v3.7",
+        event = "VeryLazy",
+        config = function()
+        end,
+    },
+    {
+        "junegunn/gv.vim",
+        event = "VeryLazy",
+        config = function()
+        end,
+    },
+    {
+        "sindrets/diffview.nvim",
+        event = "VeryLazy",
+        config = diffview,
+    },
+    {
+        "tpope/vim-rhubarb",
+        event = "VeryLazy",
+        config = function()
+        end,
+    }
 }
