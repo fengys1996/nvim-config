3db73140a8b04f67ff11f70fd17a65a1d2fe0c57
fys <fengys1996@gmail.com>
2026-01-24 13:12:07 +0800

    fix some

--- lua/config.lua
+++ lua/config.lua
@@ -2,14 +2,23 @@
 
 local m = {}
 
-m.language_server_bin_path = "/home/fys/.local/share/nvim/mason/"
-m.nvim_rs_path = "/home/fys/.config/nvim-rs"
-m.rustaceanvim_prefer_workspace_roots = {
-    "/home/fys/source/greptimedb-enterprise/",
-    "/home/fys/projects/greptimedb-enterprise/",
-    "/home/fys/projects/greptimedb/",
-    "/home/fys/projects/ent/",
-}
-m.rust_analyzer_path_backup = "/home/fys/soft/rust-analyzer/ra-25-08-25"
+if vim.g.is_linux then
+    m.language_server_bin_path = "/home/fys/.local/share/nvim/mason/"
+    m.nvim_rs_path = "/home/fys/.config/nvim-rs"
+    m.rustaceanvim_prefer_workspace_roots = {
+        "/home/fys/source/greptimedb-enterprise/",
+        "/home/fys/projects/greptimedb-enterprise/",
+        "/home/fys/projects/greptimedb/",
+        "/home/fys/projects/ent/",
+    }
+    m.rust_analyzer_path_backup = "/home/fys/soft/rust-analyzer/ra-25-08-25"
+end
+
+if vim.g.is_mac then
+    m.language_server_bin_path = "/User/fengyangsen/.local/share/nvim/mason/"
+    m.nvim_rs_path = "/User/fengyangsen/.config/nvim-rs"
+    m.rustaceanvim_prefer_workspace_roots = {}
+    m.rust_analyzer_path_backup = "/User/fengyangsen/soft/rust-analyzer/ra-25-08-25"
+end
 
 return m
