2e0c81c87636520b2b0ad0f9f97c1b06408d50fc
fys <fengys1996@gmail.com>
2026-01-24 12:47:51 +0800

    add some plugins

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -1,3 +1,18 @@
+-- local function open_win_config_func()
+--     local scr_w = vim.opt.columns:get()
+--     local scr_h = vim.opt.lines:get()
+--     local tree_w = 80
+--     local tree_h = math.floor(tree_w * scr_h / scr_w)
+--     return {
+-- 	border = "double",
+-- 	relative = "editor",
+-- 	width = tree_w,
+-- 	height = tree_h,
+-- 	col = (scr_w - tree_w) / 2,
+-- 	row = (scr_h - tree_h) / 2
+--     }
+-- end
+
 local function on_attach(bufnr)
 	local api = require('nvim-tree.api')
 
@@ -72,6 +87,10 @@ local opts = {
 		number = false,
 		relativenumber = false,
 		signcolumn = "yes",
+	    --    float = {
+	    --     enable = true,
+	    --     open_win_config = open_win_config_func
+	    -- },
 	},
 	hijack_directories = {
 		enable = true,
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -186,4 +186,71 @@ return {
             },
         },
     },
+    -- no-neck-pain and zen-mode, who is better?
+    {
+        "shortcuts/no-neck-pain.nvim",
+        version = "*",
+    },
+    {
+        "folke/zen-mode.nvim",
+        opts = {
+            -- your configurafion comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    },
+    {
+        'stevearc/oil.nvim',
+        ---@module 'oil'
+        ---@type oil.SetupOpts
+        opts = {},
+        -- Optional dependencies
+        dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+        -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+        -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+        lazy = false,
+    },
+    ---@type LazySpec
+    {
+        "mikavilpas/yazi.nvim",
+        version = "*", -- use the latest stable version
+        event = "VeryLazy",
+        dependencies = {
+            { "nvim-lua/plenary.nvim", lazy = true },
+        },
+        keys = {
+            {
+                "<leader>-",
+                mode = { "n", "v" },
+                "<cmd>Yazi<cr>",
+                desc = "Open yazi at the current file",
+            },
+            {
+                -- Open in the current working directory
+                "<leader>cw",
+                "<cmd>Yazi cwd<cr>",
+                desc = "Open the file manager in nvim's working directory",
+            },
+            {
+                "<c-up>",
+                "<cmd>Yazi toggle<cr>",
+                desc = "Resume the last yazi session",
+            },
+        },
+        ---@type YaziConfig | {}
+        opts = {
+            -- if you want to open yazi instead of netrw, see below for more info
+            open_for_directories = false,
+            keymaps = {
+                show_help = "<f1>",
+            },
+        },
+        -- 👇 if you use `open_for_directories=true`, this is recommended
+        init = function()
+            -- mark netrw as loaded so it's not loaded at all.
+            --
+            -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+            vim.g.loaded_netrwPlugin = 1
+        end,
+    }
 }
