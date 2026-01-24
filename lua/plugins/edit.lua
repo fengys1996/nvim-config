e9fca7eb60b208e7b0e62191ecca8586d6a0c057
fys <fengys1996@gmail.com>
2026-01-24 23:00:50 +0800

    use nvim-surround

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -127,10 +127,12 @@ return {
         config = config_hop,
     },
     {
-        "tpope/vim-surround",
+        "kylechui/nvim-surround",
+        version = "^3.0.0",
         event = "VeryLazy",
         config = function()
-        end,
+            require("nvim-surround").setup({})
+        end
     },
     {
         "windwp/nvim-autopairs",
