dc8043c2fd8b1efdec4d93fbb21bbad79c46ae9a
fengys1996 <fengys1996@gmail.com>
2026-04-10 10:42:28 +0800

    add ts config

--- init.lua
+++ init.lua
@@ -23,10 +23,5 @@ require("custom.colorcolumn").setup()
 vim.o.runtimepath = vim.o.runtimepath .. "," .. require("config").nvim_rs_path
 require("neovide")
 require("lspinit")
+require("ts")
 
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "rust",
-    callback = function(args)
-        vim.treesitter.start(args.buf, "rust")
-    end,
-})
---
+++ lua/ts.lua
@@ -0,0 +1,19 @@
+local sysname = vim.loop.os_uname().sysname
+local path
+
+if sysname == "FreeBSD" then
+    path = "/usr/local/lib/libtree-sitter-rust.so"
+elseif sysname == "Linux" then
+    path = "/usr/lib/libtree-sitter-rust.so"
+end
+
+if path and vim.fn.filereadable(path) == 1 then
+    vim.treesitter.language.add("rust", { path = path })
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "rust",
+    callback = function(args)
+        vim.treesitter.start(args.buf, "rust")
+    end,
+})
